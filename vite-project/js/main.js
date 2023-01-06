9b267a7391e475166dacb91a287dd380c0e0f460
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2023-01-06 10:15:44 -0500

    .

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,35 @@
 import "../styles/style.css";
 
-console.log("start");
-setTimeout(() => {
-  console.log("Timer");
-}, 5000);
-console.log("end");
+// console.log("start");
+// setTimeout(() => {
+//   console.log("Timer");
+// }, 5000);
+// console.log("end");
+
+// function greet(name) {
+//   const greetPromise = new Promise(function (resolve, reject) {
+//     resolve(`Hello ${name}`);
+//   });
+//   return greetPromise;
+// }
+// const sophie = greet("Sophie");
+// sophie.then((result) => {
+//   console.log(result);
+// });
+
+const URL = "https://api.quotable.io/random";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json(); //makes the data into JSON object so we can use
+    document.getElementById("api-response").insertAdjacentHTML(
+      "afterbegin",
+      `<h3>Quote of the Day:</h3>
+        <p>${data.content}</p>`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
