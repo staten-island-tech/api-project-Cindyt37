dd727374b759f9d2f431038febd0ed29f0b68396
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2023-03-02 09:39:52 -0500

    .

--- vite-project/js/search.js
+++ vite-project/js/search.js
@@ -5,29 +5,32 @@ const DOMSelectors = {
   searchInput: document.querySelector(".search-bar"),
   results: document.querySelector(".searched-character"),
   input: document.getElementById("searchInput"),
+  search_output: document.getElementById("searching-results"),
 };
 
-async function getData(id) {
+async function getData(characterID) {
   try {
-    const response = await fetch(`https://api.disneyapi.dev/character/${id}`);
+    const response = await fetch(
+      `https://api.disneyapi.dev/characters/${characterID}`
+    );
     if (response.status < 200 || response.status > 299) {
       throw error(response);
     } else {
       const data = await response.json();
       console.log(data);
       function displaySearch() {
-        DOMSelectors.results.insertAdjacentHTML(
+        DOMSelectors.search_output.insertAdjacentHTML(
           "beforeend",
-          `<div class= "character-card" id="${character.name}">
-            <div class="character-imgBox">
-                  <img class="character-img" src=${character.imageUrl} alt="${character.name}"></div>
-                  <div class="character-words">              
-                  <h3 class= "character-name">${character.name}</h3>
-                  <h3 class="character-info-title">Films/TV Shows ${character.name} is in:</h3>
-                  <h3 class="character-info">${character.tvShows}</h3>
-                  <h3 class="character-info">${character.films}</h3>
-                  <h5 class="character-link"><a href="https://api.disneyapi.dev/${character.name}/137">https://api.disneyapi.dev/${character.name}/137</a></h5></div>  
-              </div>`
+          `<div class= "character-card" id="${data.name}">
+          <div class="character-imgBox">
+                <img class="character-img" src=${data.imageUrl} alt="${data.name}"></div>
+                <div class="character-words">              
+                <h3 class= "character-name">${data.name}</h3>
+                <h3 class="character-info-title">Films/TV Shows ${data.name} is in:</h3>
+                <h3 class="character-info">${data.tvShows}</h3>
+                <h3 class="character-info">${data.films}</h3>
+                <h5 class="character-link"><a href="https://api.disneyapi.dev/characters/${data._id}">https://api.disneyapi.dev/characters/${data._id}</a></h5></div>  
+            </div>`
         );
       }
       displaySearch();
@@ -36,7 +39,7 @@ async function getData(id) {
     console.log(error);
     DOMSelectors.search_output.insertAdjacentHTML(
       "beforeend",
-      `<div class="error-msg">Search error occur, please try again later.</div>`
+      `<div class="error-msg">Sorry, the ID you inputed cannot be found. Please check and try again later.</div>`
     );
   }
 }
