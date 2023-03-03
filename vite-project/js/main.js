4928d58f42aeccd46fd6531be5a2cc8cc18c6c9b
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2023-03-03 09:55:06 -0500

    .

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,37 +1,48 @@
 import "../styles/style.css";
 
-const pgNumber = Math.floor(Math.random() * 149 + 1);
-const URL = "https://api.disneyapi.dev/characters?page=" + pgNumber;
-const response = await fetch(URL);
-const data = await response.json();
-console.log(data);
+const DOMSelectors = {
+  form: document.querySelector(".search-form"),
+  searchInput: document.querySelector(".search-bar"),
+  results: document.querySelector(".searched-character"),
+  input: document.getElementById("searchInput"),
+  search_output: document.getElementById("api-response"),
+};
 
-async function getData(URL) {
+async function getData(characterID) {
   try {
-    const response = await fetch(URL);
-    const data = await response.json(); //makes the data into JSON object so we can use
-    function displayCharacters() {
-      data.data.forEach((character) => {
-        document.getElementById("api-response").insertAdjacentHTML(
-          "afterbegin",
-          `<div class= "character-card" id="${character.name}">
+    const response = await fetch(
+      `https://api.disneyapi.dev/characters/${characterID}`
+    );
+    const data = await response.json();
+    console.log(data);
+    function displaySearch() {
+      DOMSelectors.search_output.insertAdjacentHTML(
+        "afterbegin",
+        `<div class= "character-card" id="${data.name}">
           <div class="character-imgBox">
-                <img class="character-img" src=${character.imageUrl} alt="${character.name}"></div>
+                <img class="character-img" src=${data.imageUrl} alt="${data.name}"></div>
                 <div class="character-words">              
-                <h3 class= "character-name">${character.name}</h3>
-                <h3 class="character-info-title">Films/TV Shows ${character.name} is in:</h3>
-                <h3 class="character-info">${character.tvShows}</h3>
-                <h3 class="character-info">${character.films}</h3>
-                <h6 class="character_ID">Character ID: ${character._id}</h6>
-                </div>  
+                <h3 class= "character-name">${data.name}</h3>
+                <h3 class="character-info-title">Films/TV Shows ${data.name} is in:</h3>
+                <h3 class="character-info">${data.tvShows}</h3>
+                <h3 class="character-info">${data.films}</h3>
+                <h5 class="character-link"><a href="https://api.disneyapi.dev/characters/${data._id}">https://api.disneyapi.dev/characters/${data._id}</a></h5></div>  
             </div>`
-        );
-      });
+      );
     }
-    displayCharacters();
+    displaySearch();
   } catch (error) {
     console.log(error);
-    alert("An error occured.");
+    DOMSelectors.search_output.insertAdjacentHTML(
+      "beforeend",
+      `<div class="error-msg">Sorry, wrong number. There is no character with that number. Try another one.</div>`
+    );
   }
 }
-getData(URL);
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  DOMSelectors.search_output.innerHTML = "";
+  event.preventDefault();
+  getData(DOMSelectors.input.value);
+  DOMSelectors.input.value = "";
+});
